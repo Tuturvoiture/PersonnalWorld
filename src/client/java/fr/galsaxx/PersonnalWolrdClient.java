6d47c969f5cdfee0b8e1908ce27416469ee80802
GalsaxX_FR <145108538+Tuturvoiture@users.noreply.github.com>
2025-06-02 15:58:28 -1000

    void+sky island
    nouvelle dimension void et apparition nbt de l'ile via copie des nbt vers generated de minecraft.

--- src/client/java/fr/galsaxx/PersonnalWolrdClient.java
+++ src/client/java/fr/galsaxx/PersonnalWolrdClient.java
@@ -2,7 +2,7 @@ package fr.galsaxx;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class PersonnalDimensionClient implements ClientModInitializer {
+public class PersonnalWolrdClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
---
+++ src/main/java/fr/galsaxx/util/IslandGenerator.java
@@ -0,0 +1,37 @@
+package fr.galsaxx.util;
+
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.structure.StructurePlacementData;
+import net.minecraft.structure.StructureTemplate;
+import net.minecraft.structure.StructureTemplateManager;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.BlockPos;
+
+import java.util.Optional;
+
+public class IslandGenerator {
+    public static void generateIsland(ServerWorld world) {
+        BlockPos center = new BlockPos(0, 150, 0); // Hauteur 150 = sûr dans le vide
+        // Place l’île uniquement si ce n’est pas déjà fait
+        if (world.getBlockState(center).isAir()) {
+            StructureTemplateManager mgr = world.getServer().getStructureTemplateManager();
+            Optional<StructureTemplate> optionalTemplate = mgr.getTemplate(Identifier.of("minecraft", "ile_1"));
+            if (optionalTemplate.isPresent()) {
+                StructureTemplate template = optionalTemplate.get();
+                template.place(
+                        world,
+                        center,
+                        center,
+                        new StructurePlacementData(),
+                        world.getRandom(),
+                        2
+                );
+                System.out.println("[personnalworld] Île NBT générée avec succès !");
+            } else {
+                System.out.println("[personnalworld] Erreur : le fichier NBT n’a pas été trouvé !");
+            }
+        } else {
+            System.out.println("[personnalworld] L’île existe déjà à cette position.");
+        }
+    }
+}
